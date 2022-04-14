7697dd0517ecb1d9a6022f3f3bd92c26766f2edb
Yannick Baudraz <yannickbaudrazdev@gmail.com>
2022-04-14 07:50:08 +0200

    Add tests cases for list with elements

--- src/test/scala/LinkedListSpec.scala
+++ src/test/scala/LinkedListSpec.scala
@@ -21,5 +21,28 @@ class LinkedListSpec extends UnitSpec {
         assert(list.empty_?)
       }
     }
+
+    "3, 2, 1 added to head" should {
+      val list = new LinkedList[Int]
+      list.addHead(3)
+      list.addHead(2)
+      list.addHead(1)
+
+      "have size 3" in {
+        assert(list.size == 3)
+      }
+
+      "have the head 1" in {
+        assert(list.head == 1)
+      }
+
+      "have the tail 3" in {
+        assert(list.tail == 3)
+      }
+
+      "have empty_? false" in {
+        assert(!list.empty_?)
+      }
+    }
   }
 }
